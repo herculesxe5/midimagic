0610f24aa80bd16f963b6a2d4b99e2e6587d6905
adrian <adrian@joe>
2021-09-09 15:18:33 +0200

    refactor main.cpp to reflect changes

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,9 @@
 #include "rotary.h"
 #include <lcdgfx.h>
 #include "bitmaps.h"
+#include "port_group.h"
+#include "inventory.h"
+#include "sys_configs.h"
 
 namespace midimagic {
 
@@ -40,44 +43,34 @@ namespace midimagic {
 
     midi::MidiInterface<HardwareSerial> MIDI((HardwareSerial&)Serial1);
 
+    std::shared_ptr<group_dispatcher> port_master(new group_dispatcher);
     std::shared_ptr<menu_state> menu(new menu_state);
-    /*
-    output_port port0(port0_pin, ad57x4::CHANNEL_A, dac1, menu, 0);
-    output_port port1(port1_pin, ad57x4::CHANNEL_B, dac1, menu, 1);
-    output_port port2(port2_pin, ad57x4::CHANNEL_C, dac1, menu, 2);
-    output_port port3(port3_pin, ad57x4::CHANNEL_D, dac1, menu, 3);
-    output_port port4(port4_pin, ad57x4::CHANNEL_A, dac0, menu, 4);
-    output_port port5(port5_pin, ad57x4::CHANNEL_B, dac0, menu, 5);
-    output_port port6(port6_pin, ad57x4::CHANNEL_C, dac0, menu, 6);
-    output_port port7(port7_pin, ad57x4::CHANNEL_D, dac0, menu, 7);
+    std::shared_ptr<menu_action_queue> action_queue(new menu_action_queue(menu));
 
-    identic_output_demux demux0;
-    */
-    rotary rot(rot_dt, rot_sw, menu);
+    std::shared_ptr<inventory> invent(new inventory(port_master, action_queue, dac0, dac1, default_config));
+
+    rotary rot(rot_dt, rot_sw, action_queue);
 
     const SPlatformI2cConfig display_config = (SPlatformI2cConfig)
-                                          { .busId = 2,
-                                            .addr = 0x3C,
-                                            .scl = disp_scl,
-                                            .sda = disp_sca,
-                                            .frequency = 0 };
+                                            { .busId = 2,
+                                              .addr = 0x3C,
+                                              .scl = disp_scl,
+                                              .sda = disp_sca,
+                                              .frequency = 0 };
 
     DisplaySSD1306_128x64_I2C display(-1, display_config);
-
 };
 
 void handleNoteOn(byte midi_channel, byte midi_note, byte midi_velo) {
     using namespace midimagic;
     midi_message msg(midi_message::NOTE_ON, midi_channel, midi_note, midi_velo);
-    //FIXME call port_group dispatcher
-    //demux0.add_note(msg);
+    port_master->add_message(msg);
 }
 
 void handleNoteOff(byte midi_channel, byte midi_note, byte midi_velo) {
     using namespace midimagic;
     midi_message msg(midi_message::NOTE_OFF, midi_channel, midi_note, midi_velo);
-    //FIXME call port_group dispatcher
-    //demux0.remove_note(msg);
+    port_master->add_message(msg);
 }
 
 void rot_clk_isr() {
@@ -100,14 +93,6 @@ void setup() {
     MIDI.begin(MIDI_CHANNEL_OMNI);
     dac0.set_level(0, ad57x4::ALL_CHANNELS);
     dac1.set_level(0, ad57x4::ALL_CHANNELS);
-    /*demux0.add_output(port0);
-    demux0.add_output(port1);
-    demux0.add_output(port2);
-    demux0.add_output(port3);
-    demux0.add_output(port4);
-    demux0.add_output(port5);
-    demux0.add_output(port6);
-    demux0.add_output(port7);*/
     // Set up interrupts
     pinMode(rot_dt, INPUT_PULLUP);
     pinMode(rot_clk, INPUT_PULLUP);
@@ -124,11 +109,12 @@ void setup() {
     display.printFixed(0, 42, "raumschiffgeraeusche", STYLE_ITALIC);
     // Wait 3 sec before display refresh
     delay(3000);
-    auto v = std::make_shared<over_view>(display, menu);
+    auto v = std::make_shared<over_view>(display, menu, invent);
     menu->register_view(v);
 }
 
 void loop() {
     using namespace midimagic;
     MIDI.read();
+    action_queue->exec_next_action();
 }

fb357a771a62dce4db2ee5523e37fb1022a3434e
adrian <adrian@joe>
2022-02-26 16:25:04 +0100

    add license header

--- src/ad57x4.cpp
+++ src/ad57x4.cpp
@@ -1,3 +1,24 @@
+/******************************************************************************
+ *                                                                            *
+ * Copyright 2020 Lukas Jünger                                                *
+ *                                                                            *
+ * This file is part of Midimagic.                                            *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify it    *
+ * under the terms of the GNU Lesser General Public License as published by   *
+ * the Free Software Foundation, either version 3 of the License,             *
+ * or (at your option) any later version.                                     *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                       *
+ * See the GNU Lesser General Public License for more details.                *
+ *                                                                            *
+ * You should have received a copy of the GNU Lesser General Public License   *
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.      *
+ *                                                                            *
+ ******************************************************************************/
+
 #include "ad57x4.h"
 #define REG_OUTPUT_RANGE_MASK 0x08
 #define REG_POWER_CTRL_MASK   0x10
